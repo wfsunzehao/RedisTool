3cb698ce158404c10836cb6cc8e97f29cfde9fe4
Youjun Zhou (WICRESOFT NORTH AMERICA LTD) <v-youjunzhou@microsoft.com>
2024-09-24 10:55:00 +0800

    first test

--- client/src/features/catalog/CompanyDetail.tsx
+++ client/src/features/catalog/CompanyDetail.tsx
@@ -1,6 +1,7 @@
 import { Typography } from "@mui/material";
 
 export default function CompanyDetail() {
+  
   return(
     <Typography variant="h2" component="h2" sx={{textAlign: 'center'}}>
       This is the CompanyDetail page
