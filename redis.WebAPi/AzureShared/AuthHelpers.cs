b72d3a000d001944ab4c9783f1ed042555eed508
zehao <v-zehaosun@microsoft.com>
2024-09-25 15:13:15 +0800

    add Azure client

---
+++ redis.WebAPi/AzureShared/AuthHelpers.cs
@@ -0,0 +1,111 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Identity.Client;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Shared
+{
+    public class AuthHelpers
+    {
+        public static async Task<X509Certificate2> GetCertificateFromKeyVaultAsync(string certificateName,
+            Uri clientCertKeyVaultUri, 
+            string tenantId)
+        {
+            RedisFunctionalTestKeyVaultObjectsRetriever keyVaultObjectsRetriever = new RedisFunctionalTestKeyVaultObjectsRetriever(tenantId, clientCertKeyVaultUri);
+            TokenCredential credential = new DefaultAzureCredential();
+            return await keyVaultObjectsRetriever.GetCertificateWithCredentialAsync(certificateName, credential);
+
+        }
+
+        public static X509Certificate2 GetX509Certificate2GenevaMetrics(Uri keyVaultUri)
+        {
+            try
+            {
+                SecretClient secretClient = new(vaultUri: keyVaultUri, credential: new DefaultAzureCredential());
+                KeyVaultSecret secret = secretClient.GetSecret("BenchmarkToolGeneva");
+                return new X509Certificate2(Convert.FromBase64String(secret.Value));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static async Task<string> GetAccessTokenAsync(string clientId,
+            string secret,
+            string tenantId,
+            string authContextPrefixUrl = "https://login.windows.net/",
+            string resourceUrl = "https://management.azure.com/")
+        {
+            try
+            {
+                string authContextURL = authContextPrefixUrl + tenantId;
+
+                ConfidentialClientApplicationBuilder confidentialClientApplicationBuilder = ConfidentialClientApplicationBuilder.Create(clientId)
+                                                                                                       .WithTenantId(tenantId)
+                                                                                                       .WithAuthority(authContextURL)
+                                                                                                       .WithClientSecret(secret);
+                string[] scopes = new[] { $"{resourceUrl}/.default" };
+                AuthenticationResult authenticationResult = await confidentialClientApplicationBuilder.Build().AcquireTokenForClient(scopes)
+                        .WithSendX5C(true)
+                        .ExecuteAsync(CancellationToken.None);
+
+                return authenticationResult.AccessToken;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static async Task<string> GetAccessToken(string clientId,
+            X509Certificate2 certificate,
+            string tenantId,
+            string authContextPrefixUrl = "https://login.windows.net/",
+            string resourceUrl = "https://management.azure.com/")
+        {
+            try
+            {
+                string authContextURL = authContextPrefixUrl + tenantId;
+
+                var tokenCredential = new ClientCertificateCredential(tenantId, clientId, certificate);
+
+                ConfidentialClientApplicationBuilder confidentialClientApplicationBuilder = ConfidentialClientApplicationBuilder.Create(clientId)
+                                                                                                       .WithTenantId(tenantId)
+                                                                                                       .WithAuthority(authContextURL)
+                                                                                                       .WithCertificate(certificate);
+
+                string[] scopes = new[] { $"{resourceUrl}/.default" };
+                AuthenticationResult authenticationResult = await confidentialClientApplicationBuilder.Build().AcquireTokenForClient(scopes)
+                        .WithSendX5C(true)
+                        .ExecuteAsync(CancellationToken.None);
+
+                return authenticationResult.AccessToken;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    
+        public static X509Certificate2 GetFunctionalTestCertFromLocalStore(string certificateSubjectName)
+        {
+            X509Store store = new(StoreName.My, StoreLocation.LocalMachine);
+
+            try
+            {
+                store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
+
+                return store.Certificates
+                    .Find(X509FindType.FindBySubjectName, certificateSubjectName, validOnly: false)
+                    .OfType<X509Certificate2>()
+                    .FirstOrDefault();
+            }
+            finally
+            {
+                store.Close();
+            }
+        }
+    }
+}
---
+++ redis.WebAPi/AzureShared/RedisFunctionalTestKeyVaultObjectsRetriever.cs
@@ -0,0 +1,91 @@
+﻿using Azure.Identity;
+using Azure;
+using System.Security.Cryptography.X509Certificates;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Core;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Shared
+{
+    public interface IRedisFunctionalTestKeyVaultObjectsRetriever
+    {
+        Task<X509Certificate2> GetCertificateWithCredentialAsync(string certificateName, TokenCredential tokenCredential);
+
+        Task<X509Certificate2> GetCertificateWithThumbprintAsync(string certificateName, string cloudTestClientId, string certThumbprint);
+
+        Task<string> GetSecretWithCredentialAsync(string secretName, TokenCredential tokenCredential);
+
+        Task<string> GetSecretWithThumbprintAsync(string secretName, string cloudTestClientId, string certThumbprint);
+    }
+
+    public class RedisFunctionalTestKeyVaultObjectsRetriever : IRedisFunctionalTestKeyVaultObjectsRetriever
+    {
+
+        private readonly string _tenantId;
+        private readonly Uri _clientKeyVaultUri;
+
+        public RedisFunctionalTestKeyVaultObjectsRetriever(string tenantId, Uri clientKeyVaultUri)
+        {
+            _tenantId = tenantId;
+            _clientKeyVaultUri = clientKeyVaultUri;
+        }
+
+        public async Task<X509Certificate2> GetCertificateWithCredentialAsync(string certificateName, TokenCredential tokenCredential)
+        {
+            var secretClient = new SecretClient(_clientKeyVaultUri, tokenCredential);
+
+            Response<KeyVaultSecret> kvSecret = await secretClient.GetSecretAsync(certificateName);
+
+            var certBytes = Convert.FromBase64String(kvSecret.Value.Value);
+
+            return new X509Certificate2(certBytes);
+        }
+
+        public async Task<X509Certificate2> GetCertificateWithThumbprintAsync(string certificateName, string cloudTestClientId, string certThumbprint)
+        {
+            var secretClient = new SecretClient(_clientKeyVaultUri,
+                new ClientCertificateCredential(_tenantId, cloudTestClientId, GetCertificateFromStore(certThumbprint)));
+
+            Response<KeyVaultSecret> kvSecret = await secretClient.GetSecretAsync(certificateName);
+
+            var certBytes = Convert.FromBase64String(kvSecret.Value.Value);
+
+            return new X509Certificate2(certBytes);
+        }
+
+        public async Task<string> GetSecretWithCredentialAsync(string secretName, TokenCredential tokenCredential)
+        {
+            var secretClient = new SecretClient(_clientKeyVaultUri, tokenCredential);
+            Response<KeyVaultSecret> kvSecret = await secretClient.GetSecretAsync(secretName);
+            return kvSecret.Value.Value;
+        }
+
+        public async Task<string> GetSecretWithThumbprintAsync(string secretName, string cloudTestClientId, string certThumbprint)
+        {
+            var secretClient = new SecretClient(_clientKeyVaultUri,
+                new ClientCertificateCredential(_tenantId, cloudTestClientId, GetCertificateFromStore(certThumbprint)));
+
+            Response<KeyVaultSecret> kvSecret = await secretClient.GetSecretAsync(secretName);
+            return kvSecret.Value.Value;
+        }
+
+        private X509Certificate2 GetCertificateFromStore(string thumbprint)
+        {
+            X509Store store = new(StoreName.My, StoreLocation.LocalMachine);
+
+            try
+            {
+                store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
+
+                return store.Certificates
+                    .Find(X509FindType.FindByThumbprint, thumbprint, validOnly: false)
+                    .OfType<X509Certificate2>()
+                    .FirstOrDefault();
+            }
+            finally
+            {
+                store.Close();
+            }
+        }
+    }
+}
