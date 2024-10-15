384999a307748f18e4e262de6773cf41839f2d9b
Junru Ma <v-junruma@microsoft.com>
2024-10-15 17:28:43 +0800

    添加了创建BVT Cache的功能

--- redis.WebAPi/Controllers/CreationController.cs
+++ redis.WebAPi/Controllers/CreationController.cs
@@ -4,7 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 using redis.WebAPi.Model;
 using redis.WebAPi.Service;
 using redis.WebAPi.Service.AzureShared;
+using redis.WebAPi.Service.AzureShared.CreationFunction;
 using redis.WebAPi.Service.IService;
+using System.Data;
+using System;
 
 namespace redis.WebAPi.Controllers
 {
@@ -14,10 +17,12 @@ namespace redis.WebAPi.Controllers
     public class CreationController : ControllerBase
     {
         private readonly IRedisCollection _redisCollection;
+        private readonly ISubscriptionResourceService _subscriptionResourceService;
 
-        public CreationController(IRedisCollection redisCollection)
+        public CreationController(IRedisCollection redisCollection, ISubscriptionResourceService subscriptionResourceService)
         {
             this._redisCollection = redisCollection;
+            this._subscriptionResourceService = subscriptionResourceService;
         }
 
 
@@ -32,6 +37,73 @@ namespace redis.WebAPi.Controllers
             };
 
             _redisCollection.CreateCache(redisReques.name, opt, redisReques.group);
+            return Ok();
+        }
+
+        [HttpPost("CreateBVTCache")]
+        //目前PrivateEndpointBladeTest和CacheCreationTest以及EnterpriseTest需手动验证创建，不包括在此功能内
+        public async Task<IActionResult> CreateBVTCache([FromBody] RedisRequestModel redisReques)
+        {
+
+            _subscriptionResourceService.SetSubscriptionResource(redisReques.subscription);
+
+            for (int i = 0; i < BVTTestCaseName.BVTTestCaseNames.Length; i++) {
+                
+                string currentTestCase = BVTTestCaseName.BVTTestCaseNames[i];
+                string currentDate = DateTime.Now.ToString("MMdd");
+
+                RedisOption opt = new RedisOption();
+                
+                if (currentTestCase == "RebootBladeTest")
+                {
+                    opt = new RedisOption()
+                    {
+                        SkuName = "Premium",
+                        RegionName = "Central US EUAP",
+                        Cluster = true,
+                        MaxShards = 2,
+                        NonSSL = true,
+                    };
+                    _redisCollection.CreateCache("BVT-" + currentTestCase + "-" + currentDate, opt, redisReques.group);
+                }
+                else if (currentTestCase == "DataPersistenceBladeTest-NotPremium")
+                {
+                    opt = new RedisOption()
+                    {
+                        SkuName = "Basic",
+                        RegionName = "Central US EUAP",
+                        NonSSL = true,
+                    };
+                    _redisCollection.CreateCache("BVT-" + currentTestCase + "-" + currentDate, opt, redisReques.group);
+                }
+                else if (currentTestCase == "GeoreplicationBladeTest")
+                {
+                    opt = new RedisOption()
+                    {
+                        SkuName = "Premium",
+                        RegionName = "Central US EUAP",
+                        NonSSL = true,
+                    };
+                    _redisCollection.CreateCache("BVT-" + currentTestCase + "-CUSE-"+ currentDate, opt, redisReques.group);
+                    opt = new RedisOption()
+                    {
+                        SkuName = "Premium",
+                        RegionName = "East US",
+                        NonSSL = true,
+                    };
+                    _redisCollection.CreateCache("BVT-" + currentTestCase + "-EUS-" + currentDate, opt, redisReques.group);
+                }
+                else
+                {
+                    opt = new RedisOption()
+                    {
+                        SkuName = "Premium",
+                        RegionName = "Central US EUAP",
+                        NonSSL = true,
+                    };
+                    _redisCollection.CreateCache("BVT-" + currentTestCase + "-" + currentDate, opt, redisReques.group);
+                }
+            }
             return Ok();
         }
 
---
+++ redis.WebAPi/Model/BVTTestCaseName.cs
@@ -0,0 +1,28 @@
+﻿namespace redis.WebAPi.Model
+{
+    public static class BVTTestCaseName
+    {
+        public static readonly string[] BVTTestCaseNames = {
+        "FlushBladeTest",
+        "DataAccessConfigurationBladeTest",
+        "OverviewBladeTest",
+        "AccessKeysBladeTest",
+        "AdvancedSettingsBladeTest",
+        "RebootBladeTest", //Need multiple shards
+        "ScaleBladeTest",
+        "ClusterSizeBladeTest",
+        "DataPersistenceBladeTest-NotPremium", //NotPremium
+        "DataPersistenceBladeTest-Premium",
+        "ManagedIdentityBladeTest",
+        "ScheduleUpdatesBladeTest",
+        "GeoreplicationBladeTest", //Need two caches
+        "VirtualNetworkBladeTest",
+        //"PrivateEndpointBladeTest", //Could not be create
+        "FirewallBladeTest",
+        "PropertiesBladeTest",
+        "Import-ExportBladeTest",
+        "PortalOwnedBladeTest",
+        "LocalizationTest"
+        };
+    }
+}
