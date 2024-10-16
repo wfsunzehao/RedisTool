462f55ef26fd927703c42f8840f493ac5db0b1d1
Youjun Zhou (WICRESOFT NORTH AMERICA LTD) <v-youjunzhou@microsoft.com>
2024-10-16 17:31:37 +0800

    Some unimportant changes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,8 +4,8 @@ const {nextui} = require("@nextui-org/theme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",	//主要是这里
-    "./node_modules/@nextui-org/theme/dist/components/(button|ripple|spinner).js",
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/components/(table|checkbox|spacer).js"
   ],
   theme: {
     extend: {},
