7c3895a4e6fc3301f91b2b571504cebc361c6892
Youjun Zhou (WICRESOFT NORTH AMERICA LTD) <v-youjunzhou@microsoft.com>
2024-12-17 17:23:45 +0800

    Test new Branch

--- client/src/features/create/CreatePage.tsx
+++ client/src/features/create/CreatePage.tsx
@@ -5,7 +5,7 @@ import { HeartIcon } from '../../common/icon/MyIcon';
 
 // 左侧导航栏的链接
 const leftLinks = [
-  { title: 'Manual ', path: '/create/man', icon: <HeartIcon /> },
+  { title: 'Manual  ', path: '/create/man', icon: <HeartIcon /> },
   { title: 'Bvt', path: '/create/bvt', icon: <Assignment /> },
   { title: 'Performance ', path: '/create/perf', icon: <Assignment /> },
   { title: "Alt", path: "/create/alt", icon: <Assignment /> },
